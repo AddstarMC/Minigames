674e8bb39b9cfe727910596ed175005e3aa76b2d
Razz0991 <paul.dav.1991@gmail.com>
2013-09-08 19:04:30 +1000

    Possible fix for NoCheat

--- src/com/pauldavdesign/mineauz/minigames/Events.java
+++ src/com/pauldavdesign/mineauz/minigames/Events.java
@@ -310,7 +310,7 @@ public class Events implements Listener{
 		}
 	}
 	
-	@EventHandler(ignoreCancelled = true)
+	@EventHandler
 	public void onTeleportAway(PlayerTeleportEvent event){
 		MinigamePlayer ply = pdata.getMinigamePlayer(event.getPlayer());
 		if(ply == null) return;
