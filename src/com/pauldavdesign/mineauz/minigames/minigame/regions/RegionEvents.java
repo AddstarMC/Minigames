f3af7157f216682c27b6a0e4b13dcb8f5411b637
Razz0991 <paul.dav.1991@gmail.com>
2014-04-11 20:10:07 +1000

    Added required events for region triggering.

--- src/com/pauldavdesign/mineauz/minigames/minigame/regions/RegionEvents.java
+++ src/com/pauldavdesign/mineauz/minigames/minigame/regions/RegionEvents.java
@@ -2,12 +2,15 @@ package com.pauldavdesign.mineauz.minigames.minigame.regions;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 import com.pauldavdesign.mineauz.minigames.MinigamePlayer;
 import com.pauldavdesign.mineauz.minigames.Minigames;
 import com.pauldavdesign.mineauz.minigames.PlayerData;
+import com.pauldavdesign.mineauz.minigames.events.EndMinigameEvent;
+import com.pauldavdesign.mineauz.minigames.events.JoinMinigameEvent;
+import com.pauldavdesign.mineauz.minigames.events.QuitMinigameEvent;
 import com.pauldavdesign.mineauz.minigames.minigame.Minigame;
 import com.pauldavdesign.mineauz.minigames.minigame.modules.RegionModule;
 
@@ -16,41 +19,70 @@ public class RegionEvents implements Listener{
 	private Minigames plugin = Minigames.plugin;
 	private PlayerData pdata = plugin.pdata;
 	
-	@EventHandler
-	private void playerMove(PlayerMoveEvent event){
-		MinigamePlayer ply = pdata.getMinigamePlayer(event.getPlayer());
-		if(ply == null) return;
-		if(ply.isInMinigame()){
-			Minigame mg = ply.getMinigame();
-			for(Region r : RegionModule.getMinigameModule(mg).getRegions()){
-				if(r.playerInRegion(ply)){
-					if(!r.hasPlayer(ply)){
-						r.addPlayer(ply);
-						r.execute(RegionTrigger.ENTER, ply);
-					}
+	private void executeRegionChanges(Minigame mg, MinigamePlayer ply){
+		for(Region r : RegionModule.getMinigameModule(mg).getRegions()){
+			if(r.playerInRegion(ply)){
+				if(!r.hasPlayer(ply)){
+					r.addPlayer(ply);
+					r.execute(RegionTrigger.ENTER, ply);
 				}
-				else{
-					if(r.hasPlayer(ply)){
-						r.removePlayer(ply);
-						r.execute(RegionTrigger.LEAVE, ply);
-					}
+			}
+			else{
+				if(r.hasPlayer(ply)){
+					r.removePlayer(ply);
+					r.execute(RegionTrigger.LEAVE, ply);
 				}
 			}
 		}
 	}
 	
 	@EventHandler
-	private void playerDeath(PlayerDeathEvent event){
-		MinigamePlayer ply = pdata.getMinigamePlayer(event.getEntity());
+	private void playerMove(PlayerMoveEvent event){
+		MinigamePlayer ply = pdata.getMinigamePlayer(event.getPlayer());
 		if(ply == null) return;
 		if(ply.isInMinigame()){
-//			Minigame mg = ply.getMinigame();
-//			for(Region r : RegionModule.getMinigameModule(mg).getRegions()){
-//				if(r.hasPlayer(ply)){
-//					r.removePlayer(ply);
-//					r.execute(RegionTrigger.LEAVE, ply);
-//				}
-//			}
+			Minigame mg = ply.getMinigame();
+			executeRegionChanges(mg, ply);
+		}
+	}
+	
+	@EventHandler
+	private void playerSpawn(PlayerRespawnEvent event){
+		MinigamePlayer ply = pdata.getMinigamePlayer(event.getPlayer());
+		if(ply == null) return;
+		if(ply.isInMinigame()){
+			Minigame mg = ply.getMinigame();
+			executeRegionChanges(mg, ply);
+		}
+	}
+	
+	@EventHandler(ignoreCancelled = true)
+	private void playerJoin(JoinMinigameEvent event){
+		MinigamePlayer ply = event.getMinigamePlayer();
+		if(ply == null) return;
+		Minigame mg = ply.getMinigame();
+		executeRegionChanges(mg, ply);
+	}
+	
+	@EventHandler
+	private void playerQuit(QuitMinigameEvent event){
+		MinigamePlayer ply = event.getMinigamePlayer();
+		if(ply == null) return;
+		Minigame mg = ply.getMinigame();
+		for(Region r : RegionModule.getMinigameModule(mg).getRegions()){
+			if(r.hasPlayer(ply))
+				r.removePlayer(ply);
+		}
+	}
+	
+	@EventHandler
+	private void playersEnd(EndMinigameEvent event){
+		for(MinigamePlayer ply : event.getWinners()){
+			Minigame mg = ply.getMinigame();
+			for(Region r : RegionModule.getMinigameModule(mg).getRegions()){
+				if(r.hasPlayer(ply))
+					r.removePlayer(ply);
+			}
 		}
 	}
 }
