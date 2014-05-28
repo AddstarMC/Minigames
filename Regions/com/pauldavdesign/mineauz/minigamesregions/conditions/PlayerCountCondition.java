6769f610ec2173348aa8c6c61bffdcaae84df208
Razz0991 <paul.dav.1991@gmail.com>
2014-05-29 09:58:02 +1000

    Added new conditions

---
+++ Regions/com/pauldavdesign/mineauz/minigamesregions/conditions/PlayerCountCondition.java
@@ -0,0 +1,96 @@
+package com.pauldavdesign.mineauz.minigamesregions.conditions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Event;
+
+import com.pauldavdesign.mineauz.minigames.MinigamePlayer;
+import com.pauldavdesign.mineauz.minigames.menu.Callback;
+import com.pauldavdesign.mineauz.minigames.menu.Menu;
+import com.pauldavdesign.mineauz.minigames.menu.MenuItemInteger;
+import com.pauldavdesign.mineauz.minigames.menu.MenuItemPage;
+import com.pauldavdesign.mineauz.minigamesregions.Node;
+import com.pauldavdesign.mineauz.minigamesregions.Region;
+
+public class PlayerCountCondition implements ConditionInterface {
+
+	@Override
+	public String getName() {
+		return "PLAYER_COUNT";
+	}
+
+	@Override
+	public String getCategory() {
+		return "Player Conditions";
+	}
+
+	@Override
+	public boolean useInRegions() {
+		return true;
+	}
+
+	@Override
+	public boolean useInNodes() {
+		return false;
+	}
+
+	@Override
+	public boolean checkRegionCondition(MinigamePlayer player,
+			Map<String, Object> args, Region region, Event event) {
+		
+		return false;
+	}
+
+	@Override
+	public boolean checkNodeCondition(MinigamePlayer player,
+			Map<String, Object> args, Node node, Event event) {
+		return false;
+	}
+
+	@Override
+	public Map<String, Object> getRequiredArguments() {
+		Map<String, Object> args = new HashMap<String, Object>();
+		args.put("c_playercount", 1);
+		return args;
+	}
+
+	@Override
+	public void saveArguments(Map<String, Object> args,
+			FileConfiguration config, String path) {
+		config.set(path + ".c_playercount", args.get("c_playercount"));
+	}
+
+	@Override
+	public Map<String, Object> loadArguments(FileConfiguration config,
+			String path) {
+		Map<String, Object> args = new HashMap<String, Object>();
+		args.put("c_playercount", config.getInt(path + ".c_playercount"));
+		return args;
+	}
+
+	@Override
+	public boolean displayMenu(MinigamePlayer player, Menu prev,
+			Map<String, Object> args) {
+		Menu m = new Menu(3, "Player Count", player);
+		m.addItem(new MenuItemPage("Back", Material.REDSTONE_TORCH_ON, prev), m.getSize() - 9);
+		final Map<String, Object> fargs = args;
+		m.addItem(new MenuItemInteger("Player Count", Material.SKULL_ITEM, new Callback<Integer>() {
+			
+			@Override
+			public void setValue(Integer value) {
+				fargs.put("c_playercount", value);
+			}
+			
+			@Override
+			public Integer getValue() {
+				return (Integer)fargs.get("c_playercount");
+			}
+		}, 1, null));
+		m.displayMenu(player);
+		return true;
+	}
+
+}
