b721287a836693f221fedab3f4fa59b5e139a1ad
Razz0991 <paul.dav.1991@gmail.com>
2014-08-03 09:06:24 +1000

    Stopped new line menu items from appearing when their slot is clicked

--- Minigames/src/au/com/mineauz/minigames/menu/MenuItemNewLine.java
+++ Minigames/src/au/com/mineauz/minigames/menu/MenuItemNewLine.java
@@ -1,9 +1,35 @@
 package au.com.mineauz.minigames.menu;
 
+import org.bukkit.inventory.ItemStack;
+
 public class MenuItemNewLine extends MenuItem{
 
 	public MenuItemNewLine() {
 		super("NL", null);
 	}
-
+	
+	@Override
+	public ItemStack onClick(){
+		return null;
+	}
+	
+	@Override
+	public ItemStack onRightClick(){
+		return null;
+	}
+	
+	@Override
+	public ItemStack onShiftClick(){
+		return null;
+	}
+	
+	@Override
+	public ItemStack onShiftRightClick(){
+		return null;
+	}
+	
+	@Override
+	public ItemStack onDoubleClick(){
+		return null;
+	}
 }
