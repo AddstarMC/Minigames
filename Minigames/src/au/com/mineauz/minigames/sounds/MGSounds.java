e236d6132c09e6a2b56dc13be6f1d7d946d177cd
addstar <addstar@addstar.com.au>
2016-03-08 19:54:39 +1100

    Initial update for 1.9 compatibility

--- Minigames/src/au/com/mineauz/minigames/sounds/MGSounds.java
+++ Minigames/src/au/com/mineauz/minigames/sounds/MGSounds.java
@@ -10,10 +10,10 @@ public class MGSounds {
 	private static Map<String, MGSound> sounds = new HashMap<String, MGSound>();
 	
 	static{
-		addSound("timerTick", new MGSound(Sound.NOTE_STICKS, 10f, 1.5f));
-		addSound("win", new MGSound(Sound.LEVEL_UP));
-		addSound("lose", new MGSound(Sound.NOTE_PIANO, 10f, 0.2f, 3, 5L));
-		addSound("gameStart", new MGSound(Sound.ORB_PICKUP));
+		addSound("timerTick", new MGSound(Sound.BLOCK_NOTE_HAT, 10f, 1.5f));
+		addSound("win", new MGSound(Sound.ENTITY_PLAYER_LEVELUP));
+		addSound("lose", new MGSound(Sound.BLOCK_NOTE_PLING, 10f, 0.2f, 3, 5L));
+		addSound("gameStart", new MGSound(Sound.ENTITY_EXPERIENCE_ORB_PICKUP));
 	}
 	
 	private static void addSound(String name, MGSound sound){
