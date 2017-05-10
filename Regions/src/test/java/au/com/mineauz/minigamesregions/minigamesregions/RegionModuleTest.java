526952e6fe425ea7af1a0a636b54b509c884a193
Narimm <benjicharlton@gmail.com>
2017-05-10 16:02:54 +1000

    Refactor Minigames Regions to MAVEN STANDARDS Setup Test suite

---
+++ Regions/src/test/java/au/com/mineauz/minigamesregions/minigamesregions/RegionModuleTest.java
@@ -0,0 +1,35 @@
+package au.com.mineauz.minigamesregions.minigamesregions;
+
+import au.com.mineauz.minigames.gametypes.MinigameType;
+import au.com.mineauz.minigames.minigame.Minigame;
+import au.com.mineauz.minigamesregions.RegionModule;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created for the AddstarMC Project.
+ * Created by Narimm on 10/05/2017.
+ */
+public class RegionModuleTest {
+    @Test
+    public void saveTest() throws Exception {
+    }
+
+    @Test
+    public void loadTest() throws Exception {
+        //Minigame game = new Minigame("Test");
+        //RegionModule module =  new RegionModule(game);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
