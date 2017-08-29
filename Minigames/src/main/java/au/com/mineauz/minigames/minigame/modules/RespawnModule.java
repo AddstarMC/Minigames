7a61e1736b58b03e139a6521d43cc98b415fc96c
Narimm <benjicharlton@gmail.com>
2017-08-29 12:16:24 +1000

    Add new Respawn Module to allow for respawn timing.

---
+++ Minigames/src/main/java/au/com/mineauz/minigames/minigame/modules/RespawnModule.java
@@ -0,0 +1,8 @@
+package au.com.mineauz.minigames.minigame.modules;
+
+/**
+ * Created for use for the Add5tar MC Minecraft server
+ * Created by benjamincharlton on 16/08/2017.
+ */
+public class RespawnModule {
+}
