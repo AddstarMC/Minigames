34e9d2fd299828eb3e2d73c43e626c14a670007f
Narimm <benjicharlton@gmail.com>
2019-01-04 14:37:14 +1000

    Add more testing background
    Signed-off-by: Narimm <benjicharlton@gmail.com>

--- Minigames/src/test/java/au/com/mineauz/minigames/MinigamesTest.java
+++ Minigames/src/test/java/au/com/mineauz/minigames/MinigamesTest.java
@@ -3,12 +3,16 @@ package au.com.mineauz.minigames;
 import au.com.mineauz.minigames.gametypes.MinigameType;
 import au.com.mineauz.minigames.mechanics.GameMechanics;
 import au.com.mineauz.minigames.minigame.Minigame;
+import au.com.mineauz.minigames.minigame.modules.LobbySettingsModule;
+import au.com.mineauz.minigames.minigame.modules.MinigameModule;
 import au.com.mineauz.minigames.objects.TestPlayer;
 import be.seeseemelk.mockbukkit.MockBukkit;
 import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.command.ConsoleCommandSenderMock;
 import be.seeseemelk.mockbukkit.entity.PlayerMock;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.command.ConsoleCommandSender;
 import org.junit.Before;
 import org.junit.Test;
 import org.sqlite.SQLiteDataSource;
@@ -31,11 +35,18 @@ public class MinigamesTest {
     private Minigame game;
     private World world;
     private SQLiteDataSource datasource;
+    private Location spawn;
+    private Location lobby;
+    private Location end;
+    private Location start;
+    private Location quit;
 
     @Before
     public void Setup(){
 
         server = MockBukkit.mock();
+        ConsoleCommandSenderMock sender  = (ConsoleCommandSenderMock) server.getConsoleSender();
+        sender.setOutputOnSend(true);
         MockBukkit.getMock().addSimpleWorld("GAMES");
         String ver = server.getBukkitVersion();
         Logger log = Logger.getAnonymousLogger();
@@ -44,25 +55,37 @@ public class MinigamesTest {
         plugin.toggleDebug();
         plugin.setLog(log);
         world = MockBukkit.getMock().getWorld("GAMES");
+        spawn =world.getSpawnLocation();
         createMinigame();
         player = new TestPlayer(MockBukkit.getMock(),"TestPlayer", UUID.randomUUID());
-        player.setLocation(new Location(world,0,0,0));
+        player.setOutputOnSend(true);
+        player.setLocation(spawn);
         MockBukkit.getMock().addPlayer(player);
     }
 
     private void createMinigame(){
-        game = new Minigame("TestGame",MinigameType.MULTIPLAYER,new Location(world,0,10,0));
+        start = new Location(world,0,21,0);
+        game = new Minigame("TestGame",MinigameType.MULTIPLAYER,start);
         game.setType(MinigameType.MULTIPLAYER);
         game.setMechanic(GameMechanics.MECHANIC_NAME.CTF.toString());
         game.setDeathDrops(true);
-        game.setQuitPosition(new Location(world,0,20,0));
-        game.setLobbyPosition(new Location(world,0,5.,0));
-        game.setEndPosition(new Location(world,0,21,0));
+        quit = new Location(world,0,20,0);
+        game.setQuitPosition(quit);
+        lobby= new Location(world,0,5.,0);
+        game.setLobbyPosition(lobby);
+        end = new Location(world, 0, 25, 0);
+        game.setEndPosition(end);
         game.setEnabled(true);
         game.setStartWaitTime(5);
         game.setTimer(5);
         game.setMaxScore(3);
         game.setMaxPlayers(2);
+        MinigameModule module = game.getModule("LobbySettings");
+        if(module != null) {
+            LobbySettingsModule lMod = (LobbySettingsModule) module;
+            lMod.setTeleportOnPlayerWait(true);
+            lMod.setTeleportOnStart(true);
+        }
         plugin.getMinigameManager().addMinigame(game);
     }
     
@@ -70,13 +93,18 @@ public class MinigamesTest {
     public void onJoinMinigame() {
         plugin.getPlayerManager().addMinigamePlayer(player);
         MinigamePlayer mplayer = plugin.getPlayerManager().getMinigamePlayer(player.getUniqueId());
-        System.out.println(player.getLocation().toString());
         plugin.getPlayerManager().joinMinigame(mplayer, game, false, 0D);
-        String message = player.nextMessage();
-        while (message != null) {
-            System.out.println(message);
-            message = player.nextMessage();
-        }
+        player.assertLocation(lobby,0);
         plugin.getPlayerManager().startMPMinigame(game);
+        //player.assertLocation(start,0);
+    }
+    
+    public void onQuitMinigame(){
+        plugin.getPlayerManager().addMinigamePlayer(player);
+        MinigamePlayer mplayer = plugin.getPlayerManager().getMinigamePlayer(player.getUniqueId());
+        plugin.getPlayerManager().joinMinigame(mplayer, game, false, 0D);
+        
+    
+    
     }
 }
