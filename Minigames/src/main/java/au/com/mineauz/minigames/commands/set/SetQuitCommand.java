GreenSurvivors/GreenMinigames

step 10 lang rework
"simplified" random chests command: removed true/false setting in favor of dis/enabling it via the two numeric parameters.
This way Users hopefully aren't as confused how to config this feature.

Added better time parsing for commands

Changed Reward system to match the Module one

removed label from commands it was never used

drop item rewards that doesn't fit into inventory

Minigames/src/main/java/au/com/mineauz/minigames/commands/set/SetQuitCommand.java
@@ -1,11 +1,15 @@
 package au.com.mineauz.minigames.commands.set;
 
 import au.com.mineauz.minigames.commands.ICommand;
+import au.com.mineauz.minigames.managers.MinigameMessageManager;
+import au.com.mineauz.minigames.managers.language.MinigameLangKey;
+import au.com.mineauz.minigames.managers.language.MinigameMessageType;
+import au.com.mineauz.minigames.managers.language.MinigamePlaceHolderKey;
 import au.com.mineauz.minigames.minigame.Minigame;
 import net.kyori.adventure.text.Component;
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.Entity;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,17 +34,11 @@ public class SetQuitCommand implements ICommand {
 
     @Override
     public @NotNull Component getDescription() {
-        return "Sets the quitting position of a Minigame to where you are standing.";
+        return MinigameMessageManager.getMgMessage(MinigameLangKey.COMMAND_SET_QUIT_DESCRIPTION);
     }
-
     @Override
-    public @NotNull String @Nullable [] getParameters() {
-        return null;
-    }
-
-    @Override
-    public String[] getUsage() {
-        return new String[]{"/minigame set <Minigame> quit"};
+    public Component getUsage() {
+        return MinigameMessageManager.getMgMessage(MinigameLangKey.COMMAND_SET_QUIT_USAGE);
     }
 
     @Override
@@ -49,17 +47,21 @@ public class SetQuitCommand implements ICommand {
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, Minigame minigame,
-                             @NotNull String label, @NotNull String @Nullable [] args) {
-        Player player = (Player) sender;
-        minigame.setQuitPosition(player.getLocation());
-        sender.sendMessage(ChatColor.GRAY + "Quit position has been set for " + minigame);
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Minigame minigame,
+                             @NotNull String @Nullable [] args) {
+        if (sender instanceof Entity entity) {
+            minigame.setQuitPosition(entity.getLocation());
+            MinigameMessageManager.sendMgMessage(sender, MinigameMessageType.INFO, MinigameLangKey.COMMAND_SET_QUIT_SUCCESS,
+                    Placeholder.unparsed(MinigamePlaceHolderKey.MINIGAME.getKey(), minigame.getName(false)));
+        } else {
+            MinigameMessageManager.sendMgMessage(sender, MinigameMessageType.ERROR, MinigameLangKey.COMMAND_ERROR_NOTAPLAYER);
+        }
         return true;
     }
 
     @Override
-    public @Nullable List<@NotNull String> onTabComplete(@NotNull CommandSender sender, Minigame minigame,
-                                                         String alias, @NotNull String @NotNull [] args) {
+    public @Nullable List<@NotNull String> onTabComplete(@NotNull CommandSender sender, @NotNull Minigame minigame,
+                                                         @NotNull String @NotNull [] args) {
         return null;
     }
 

Minigames/src/main/java/au/com/mineauz/minigames/managers/MinigamePlayerManager.java
@@ -23,6 +23,7 @@ import au.com.mineauz.minigames.stats.DynamicMinigameStat;
 import au.com.mineauz.minigames.stats.MinigameStats;
 import au.com.mineauz.minigames.stats.StoredGameStats;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.*;
 import org.bukkit.FireworkEffect.Type;
@@ -69,7 +70,7 @@ public class MinigamePlayerManager {
         JoinMinigameEvent event = new JoinMinigameEvent(mgPlayer, minigame);
         Bukkit.getServer().getPluginManager().callEvent(event);
         if (event.isCancelled()) {
-            Minigames.log.info("Start Event was cancelled..: " + event);
+            Minigames.getCmpnntLogger().info("Join Event was cancelled: " + event);
             return;
         }
         if (!mgManager.minigameStartStateCheck(minigame, mgPlayer)) return;
@@ -577,7 +578,7 @@ public class MinigamePlayerManager {
                     if (loc != null) {
                         mgPlayer.teleport(loc);
                     } else {
-                        Minigames.log.warning("Minigame " + minigame.getName(true) + " has no end position set! (Player: " + mgPlayer.getName() + ")");
+                        Minigames.getCmpnntLogger().warn("Minigame " + minigame.getName(true) + " has no end position set! (Player: " + mgPlayer.getName() + ")");
                     }
                 } else {
                     if (!isWinner) {
@@ -897,12 +898,12 @@ public class MinigamePlayerManager {
                             Placeholder.unparsed(MinigamePlaceHolderKey.MINIGAME.getKey(), minigame.getName(true)),
                             Placeholder.component(MinigamePlaceHolderKey.SCORE.getKey(), score)), minigame, MinigameMessageType.WIN);
                 } else if (winners.size() > 1) {
-                    Component winComp = Component.empty();
+                    TextComponent.Builder winComp = Component.text();
                     winners.sort(Comparator.comparingInt(MinigamePlayer::getScore));
 
                     for (MinigamePlayer pl : winners) {
                         if (winners.indexOf(pl) < 2) {
-                            winComp = winComp.append(Component.text(pl.getDisplayName(minigame.usePlayerDisplayNames())));
+                            winComp.append(Component.text(pl.getDisplayName(minigame.usePlayerDisplayNames())));
                             if (winners.indexOf(pl) + 2 >= winners.size()) {
                                 winComp.append(" and ");
                             } else {

AddstarMC/Minigames

Minigames/src/main/java/au/com/mineauz/minigames/commands/set/SetQuitCommand.java
@@ -1,11 +1,15 @@
 package au.com.mineauz.minigames.commands.set;
 
 import au.com.mineauz.minigames.commands.ICommand;
+import au.com.mineauz.minigames.managers.MinigameMessageManager;
+import au.com.mineauz.minigames.managers.language.MinigameLangKey;
+import au.com.mineauz.minigames.managers.language.MinigameMessageType;
+import au.com.mineauz.minigames.managers.language.MinigamePlaceHolderKey;
 import au.com.mineauz.minigames.minigame.Minigame;
 import net.kyori.adventure.text.Component;
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.Entity;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,17 +34,11 @@ public class SetQuitCommand implements ICommand {
 
     @Override
     public @NotNull Component getDescription() {
-        return "Sets the quitting position of a Minigame to where you are standing.";
+        return MinigameMessageManager.getMgMessage(MinigameLangKey.COMMAND_SET_QUIT_DESCRIPTION);
     }
-
     @Override
-    public @NotNull String @Nullable [] getParameters() {
-        return null;
-    }
-
-    @Override
-    public String[] getUsage() {
-        return new String[]{"/minigame set <Minigame> quit"};
+    public Component getUsage() {
+        return MinigameMessageManager.getMgMessage(MinigameLangKey.COMMAND_SET_QUIT_USAGE);
     }
 
     @Override
@@ -49,17 +47,20 @@ public class SetQuitCommand implements ICommand {
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, Minigame minigame,
-                             @NotNull String label, @NotNull String @Nullable [] args) {
-        Player player = (Player) sender;
-        minigame.setQuitLocation(player.getLocation());
-        sender.sendMessage(ChatColor.GRAY + "Quit position has been set for " + minigame);
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Minigame minigame,
+                             @NotNull String @Nullable [] args) {
+        if (sender instanceof Entity entity) {
+            minigame.setQuitLocation(entity.getLocation());
+            MinigameMessageManager.sendMgMessage(sender, MinigameMessageType.INFO, MinigameLangKey.COMMAND_SET_QUIT_SUCCESS,
+                    Placeholder.unparsed(MinigamePlaceHolderKey.MINIGAME.getKey(), minigame.getName(false)));
+        } else {
+            MinigameMessageManager.sendMgMessage(sender, MinigameMessageType.ERROR, MinigameLangKey.COMMAND_ERROR_NOTAPLAYER);
+        }
         return true;
     }
 
     @Override
-    public @Nullable List<@NotNull String> onTabComplete(@NotNull CommandSender sender, Minigame minigame,
-                                                         String alias, @NotNull String @NotNull [] args) {
+    public @Nullable List<@NotNull String> onTabComplete(@NotNull CommandSender sender, @NotNull Minigame minigame, @NotNull String @NotNull [] args) {
         return null;
     }
 

Minigames/src/main/java/au/com/mineauz/minigames/managers/MinigamePlayerManager.java
@@ -23,6 +23,7 @@ import au.com.mineauz.minigames.stats.DynamicMinigameStat;
 import au.com.mineauz.minigames.stats.MinigameStats;
 import au.com.mineauz.minigames.stats.StoredGameStats;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.*;
 import org.bukkit.FireworkEffect.Type;
@@ -69,7 +70,7 @@ public class MinigamePlayerManager {
         JoinMinigameEvent event = new JoinMinigameEvent(mgPlayer, minigame);
         Bukkit.getServer().getPluginManager().callEvent(event);
         if (event.isCancelled()) {
-            Minigames.log.info("Start Event was cancelled..: " + event);
+            Minigames.getCmpnntLogger().info("Join Event was cancelled: " + event);
             return;
         }
         if (!mgManager.minigameStartStateCheck(minigame, mgPlayer)) return;
@@ -621,7 +622,7 @@ public class MinigamePlayerManager {
                     if (loc != null) {
                         mgPlayer.teleport(loc);
                     } else {
-                        Minigames.log.warning("Minigame " + minigame.getName(true) + " has no end location set! (Player: " + mgPlayer.getName() + ")");
+                        Minigames.getCmpnntLogger().warn("Minigame " + minigame.getName(true) + " has no end location set! (Player: " + mgPlayer.getName() + ")");
                     }
                 } else {
                     if (!isWinner) {
@@ -915,12 +916,12 @@ public class MinigamePlayerManager {
                             Placeholder.unparsed(MinigamePlaceHolderKey.MINIGAME.getKey(), minigame.getName(true)),
                             Placeholder.component(MinigamePlaceHolderKey.SCORE.getKey(), score)), minigame, MinigameMessageType.WIN);
                 } else if (winners.size() > 1) {
-                    Component winComp = Component.empty();
+                    TextComponent.Builder winComp = Component.text();
                     winners.sort(Comparator.comparingInt(MinigamePlayer::getScore));
 
                     for (MinigamePlayer pl : winners) {
                         if (winners.indexOf(pl) < 2) {
-                            winComp = winComp.append(Component.text(pl.getDisplayName(minigame.usePlayerDisplayNames())));
+                            winComp.append(Component.text(pl.getDisplayName(minigame.usePlayerDisplayNames())));
                             if (winners.indexOf(pl) + 2 >= winners.size()) {
                                 winComp.append(" and ");
                             } else {