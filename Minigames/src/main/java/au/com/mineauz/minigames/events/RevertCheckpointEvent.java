b3823c2bcabd19bf1c08150f8cd9244601ca3826
addstar <addstar@addstar.com.au>
2024-12-20 18:31:32 +1100

    fix: set Minigame in RevertCheckpointEvent

--- Minigames/src/main/java/au/com/mineauz/minigames/events/RevertCheckpointEvent.java
+++ Minigames/src/main/java/au/com/mineauz/minigames/events/RevertCheckpointEvent.java
@@ -1,5 +1,6 @@
 package au.com.mineauz.minigames.events;
 
+import au.com.mineauz.minigames.minigame.Minigame;
 import au.com.mineauz.minigames.objects.MinigamePlayer;
 import org.bukkit.entity.Player;
 
@@ -7,7 +8,7 @@ public class RevertCheckpointEvent extends AbstractCancellableMinigameEvent {
     private final MinigamePlayer player;
 
     public RevertCheckpointEvent(MinigamePlayer player) {
-        super(null);
+        super(player.getMinigame());
         this.player = player;
     }
 
@@ -19,4 +20,8 @@ public class RevertCheckpointEvent extends AbstractCancellableMinigameEvent {
         return player.getPlayer();
     }
 
+    @Override
+    public Minigame getMinigame() {
+        return player.getMinigame() == null ? null : player.getMinigame();
+    }
 }
